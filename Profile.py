773ca9bdec86df01443e0f4857e38bc18cff8317
jayakumar2497 <d.jayakumar.1997@gmail.com>
2025-10-28 17:09:26 +0530

    Profile

---
+++ Profile.py
@@ -0,0 +1,13 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/Ajith/Documents/Python_jk/Profile.py ============
+Name: Jayakumar Duraipandi
+Date of birth : 24.09.1997
+Father Name : Duraipandi
+Mother Name : Vijayalakshmi
+Brother's Name : Ajithkumar & Aathithya 
+Address : No.77, Muthampatti , Kurihipatti (po) , Melur , Madurai-625109
+Email: d.jayakumar.1997@gmail.com
+UG : BE (ECE) 
+Language: Tamil , English
