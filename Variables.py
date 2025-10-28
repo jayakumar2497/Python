68f9587109dc9a2a391754fb588cdb7cc60ac4e8
jayakumar2497 <d.jayakumar.1997@gmail.com>
2025-10-28 17:35:07 +0530

    Uses Of Variables

---
+++ Variables.py
@@ -0,0 +1,28 @@
+name ="Jayakumar"
+print(name)
+age = 28
+print(age)
+height = 173
+print(height)
+weight = 63
+print(weight)
+city = "Melur"
+print(city)
+is_student = False
+print(is_student)
+marks =370
+print(marks)
+pi=3.14
+print(pi)
+school_name= "Don Bosco Matriculation School Chennai-52"
+print(school_name)
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+my_number=10
+print(my_number)
+print(type(my_number))
+my_text="Hello"
+print(my_text)
+print(type(my_text))
