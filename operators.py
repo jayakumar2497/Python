269082635dea93b21558997cc370f43f4cb93372
jayakumar2497 <d.jayakumar.1997@gmail.com>
2025-10-30 20:23:59 +0530

    Add files via upload
    Operators

---
+++ operators.py
@@ -0,0 +1,65 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+d=(2.4)
+SI=(a*b*c)/100
+print("add two numbers:",a+b)
+print("sub two numbers:",a-b)
+print("mul two numbers:",a*b)
+print("div two numbers:",a/b)
+print("floor div two numbers:",a//b)
+print("find remainder:",a%b)
+print("square a number:",a**2)
+print("cube a number:",a**3)
+print("Average :",(a+b+c)/3)
+print("a=",b,"b=",a)
+print(type(d))
+print(a+d)
+print(a>b)
+print(a==b)
+Sub_1=int(input("enter mark:"))
+Sub_2=int(input("enter mark:"))
+Sub_3=int(input("enter mark:"))
+Sub_4=int(input("enter mark:"))
+Sub_5=int(input("enter mark:"))
+tot=Sub_1+Sub_2+Sub_3+Sub_4+Sub_5
+avg=tot/5
+print("Total:",tot)
+print("Average:",avg)
+#convert hours to minutes
+print("hours to minutes")
+hours=float(input("enter the number of hours:"))
+minutes=hours*60
+print("Hours to Minutes:",minutes)
+#Simple Interest
+print(SI)
+#Area of Rectangle
+l=int(input("Enter length:" ))
+b=int(input("Enter breadth:" ))
+print(l*b)
+Number=int(input("Enter the Number:"))
+if(Number>0):
+    print("Positive")
+else:
+    print("Negative")
+print()
+#calculate Total Cost
+print("Total Cost")
+COI=int(input("Enter your cost of item:"))
+NOI=int(input("Enter your Number of item:"))
+print("Total cost",COI*NOI)
+#Calculator
+print("calculator")
+num1=int(input("Enter ur Number"))
+num2=int(input("Enter ur Number"))
+print("Addition:",num1+num2)
+print("Subtraction:",num1+num2)
+print("Multiplication:",num1+num2)
+print("Division:",num1+num2)
+
+print("Thank You By JK")
+
+
+
+
+            
